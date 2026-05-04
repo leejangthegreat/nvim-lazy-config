f045c3b3bc40b14d5df87a28f04385fc43d2827f
Hao-Tian Li <109223600+leejangthegreat@users.noreply.github.com>
2026-05-04 14:21:19 +0800

    Add lsp struct to statusline

--- lua/lee/plugins/statusline.lua
+++ lua/lee/plugins/statusline.lua
@@ -1,55 +1,58 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = {
-		"nvim-tree/nvim-web-devicons"
+		"nvim-tree/nvim-web-devicons",
+		"stevearc/aerial.nvim",
 	},
 	config = function()
 		local luaLine = require("lualine")
-		local lazyStatus = require("lazy.status")  -- Configure lazy pending updates count
+		local lazyStatus = require("lazy.status") -- Configure lazy pending updates count
 
 		-- Colors for different mode can be set here
 
-
 		-- Components
 		-- Mode indicators
---		local mode = {
---			'mode', fmt = function(str)
---				return " " .. str
---			end,
---		}
+		--		local mode = {
+		--			'mode', fmt = function(str)
+		--				return " " .. str
+		--			end,
+		--		}
 
 		local mode = {
-			'mode', fmt = function(str)
+			"mode",
+			fmt = function(str)
 				return str
 			end,
 		}
 
 		local diff = {
-			'diff',
+			"diff",
 			colored = true,
 			symbols = {
-				added = ' ', modified = ' ', removed = ' '
-			}
+				added = " ",
+				modified = " ",
+				removed = " ",
+			},
 		}
 
 		local filename = {
-			'filename',
+			"filename",
 			file_status = true,
-			path = 0,  -- Just show filename
+			path = 0, -- Just show filename
 		}
 
 		local branch = {
-			'branch',
+			"branch",
 			icon = { "", color = { fg = "#A6D4DE" } },
-			'|'
+			"|",
 		}
 
 		luaLine.setup({
 			icons_enabled = true,
 			options = {
-				theme=my_lualine_theme,
-				component_separators = { left = '|', right = '|' },
-				section_separators = { left = '', right = '' }
+				theme = my_lualine_theme,
+				component_separators = { left = "|", right = "|" },
+				section_separators = { left = "", right = "" },
 			},
 			sections = {
 				lualine_a = { mode },
@@ -61,9 +64,16 @@ return {
 						cond = lazyStatus.has_updates,
 						color = { fg = "#FF9E64" },
 					},
-					{ "filetype" },
+					{
+						"aerial",
+						sep = " > ",
+						depth = nil,
+						dense = false,
+						dense_sep = ".",
+						colored = true,
+					},
 				},
 			},
 		})
-	end
+	end,
 }
